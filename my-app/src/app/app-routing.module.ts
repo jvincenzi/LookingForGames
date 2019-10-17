1e6c54662961e2895e9c91591002a8a8cad49ca2
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2019-10-17 09:26:19 -0700

    popup

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { GameBoardComponent } from './game-board/game-board.component';
+import { UsersComponent } from './users/users.component';
+
 
 const myRoutes: Routes = [
   { path: 'signIn', component: SignInComponent },
-  { path: 'gameBoard', component: GameBoardComponent }
+  { path: 'gameBoard', component: GameBoardComponent },
+  {path: 'users', component: UsersComponent}
+ // { path: 'users', component: UsersComponent }
  ];
  
 
