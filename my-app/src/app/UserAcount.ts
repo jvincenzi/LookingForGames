ce52067112e057c584e7a39f2bc505ee96c39d5c
Joseph Vincenzi <j.vincenzi@bellevuecollege.edu>
2019-10-20 21:28:02 -0700

    added UserAcount.ts class and payment-auth.component.html

---
+++ my-app/src/app/UserAcount.ts
@@ -0,0 +1,19 @@
+export class UserAcount {
+    _id: String;
+    FirstName: string;
+    LastName: string;
+    Email: string;
+    UserName: string;
+    Password: string;
+    Telephone: string;
+    DateOfBirth: Date;
+    Address: string;
+    City: string;
+    State: string;
+    Zipcode:  number;
+    FreeAccount: boolean;
+
+
+
+    createdOn: Date;
+  }
