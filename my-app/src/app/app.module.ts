7c480227d8d62f7d732e6f8b99cc4ff4d570339a
SeanNewman22 <sean.m.newman@comcast.net>
2019-10-27 09:46:11 -0700

    Made an angular material nav bar

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from  '@angular/material';
 
 import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
@@ -9,20 +11,17 @@ import { ToDoComponent } from './to-do/to-do.component';
 import { WordSortComponent } from './word-sort/word-sort.component';
 import { CounterComponent } from './counter/counter.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatSelectModule} from '@angular/material/select';
 import { GameBoardComponent } from './game-board/game-board.component';
 import { UsersComponent } from './users/users.component';
 import { BannerAdComponent } from './banner-ad/banner-ad.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { AppRoutingModule } from './app-routing.module';
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { PaymentAuthComponent } from './payment-auth/payment-auth.component';
 import { ShowUserInfoComponent } from './show-user-info/show-user-info.component';
 
-
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -42,14 +41,16 @@ import { ShowUserInfoComponent } from './show-user-info/show-user-info.component
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule,
     BrowserAnimationsModule,
-    MatSliderModule,
-    MatSelectModule,
+    HttpClientModule,
     AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
     ReactiveFormsModule
-  
   ],
   providers: [],
   bootstrap: [AppComponent]
