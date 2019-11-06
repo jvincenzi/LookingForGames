b1b9e4245a2b60dcb600d9fcc0f3dd30f00a3479
SeanNewman22 <sean.m.newman@comcast.net>
2019-11-05 18:44:14 -0800

    Updated app for azure

--- my-app/src/app/User-list.ts
+++
@@ -1,8 +0,0 @@
-
-import { Users } from './Users';
-export const USERLIST: Users[] = [
- { UserID: 1, FirstName: "Sean", LastName: "Newsman",   CurrentStatus: "Online", Location:"Bellevue"},
- { UserID: 2, FirstName: "Thanh", LastName: "Duong",   CurrentStatus: "OffLine", Location:"Seattle"},
- { UserID: 3, FirstName: "Joseph", LastName: "Vincenzi.",   CurrentStatus: "OffLine", Location:"Bellevue"}
-];
-
