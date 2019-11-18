d51ec5c1716ccb13025a9f4dc78507febade5580
Joseph Vincenzi <joevincenzi@gmail.com>
2019-11-17 20:57:00 -0800

    Fix round 3.1

--- nodeServer/app.js
+++ nodeServer/app.js
@@ -8,8 +8,8 @@ const cors = require('cors')
 
 var corsOptions = {
   // this URL must match the URL that the Angular app will call from
-  //origin: 'http://localhost:4200', /////////////////////////////// LocalHost Config ///////////////////////////////  
-  origin: 'lookingforgames.azurewebsites.net/', 
+  origin: 'http://localhost:4200', /////////////////////////////// LocalHost Config ///////////////////////////////  
+  //origin: 'lookingforgames.azurewebsites.net/', 
   optionsSuccessStatus: 200 
 }
 
