2eba92af9da38e0e264cc8106c600117908966d0
Joseph Vincenzi <j.vincenzi@bellevuecollege.edu>
2019-11-19 13:17:20 -0800

    added dataStore.ts file class object

---
+++ my-app/src/app/DataStore.ts
@@ -0,0 +1,31 @@
+export class DataStore {
+    sessionTokenData: {
+      _id: string;
+      UserName: string;
+      Password: string;
+      Email: string;
+      FirstName: string;
+      LastName: string;
+      Telephone: string;
+      DateOfBirth: Date;
+      Country: string; // new
+      Address: string;
+      Address2: string; // new
+      City: string;
+      State: string;
+      Zipcode: number;
+      FreeAccount: boolean;
+      SubscriptionExp: Date;
+      SubscriptionLv: number;//level (0=freeAccount, 1=monthly or 12=yearly)
+      AdminAccess: string;  // new
+      CurrentStatus: string;
+      Location: string;
+      createdOn: string;
+      UID: string; // new
+      userLatitude: number; // new
+      userLongitude: number; // new
+    };
+    searchBar: {
+        selectedGame: string;
+    };    
+  }
