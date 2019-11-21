b6db003b4f7942b043c381e399ab0e1b685a1b60
Joseph Vincenzi <j.vincenzi@bellevuecollege.edu>
2019-11-20 17:37:13 -0800

    karma tests for search-bar.component.spec.ts are passing

--- my-app/src/app/search-bar/search-bar.component.spec.ts
+++ my-app/src/app/search-bar/search-bar.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchBarComponent } from './search-bar.component';
+import { MatFormFieldModule, MatSelectModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
@@ -8,7 +12,21 @@ describe('SearchBarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchBarComponent ]
+      imports: [
+        RouterTestingModule,
+        //MatIconModule,
+        //MatToolbarModule,
+        //MatSidenavModule,
+        //MatListModule,
+        //MatButtonModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        FormsModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [ 
+        SearchBarComponent 
+      ]
     })
     .compileComponents();
   }));
