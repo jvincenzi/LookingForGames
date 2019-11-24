6c5ec031665b3549c493c28deb29e9158c52b6ba
SeanNewman22 <sean.m.newman@comcast.net>
2019-11-23 17:26:33 -0800

    implementing kurt's way of calling google api

--- nodeServer/controllers/GoogleController.js
+++ nodeServer/controllers/GoogleController.js
@@ -1,27 +1,42 @@
+const GoogleDistanceApi = require('google-distance-api');
+
+exports.getDistance = (req, res) => {
+    console.log(">>>>>>>>> IN GET DISTANCE <<<<<<<<<");
+
+    const options = {
+      key: 'AIzaSyABCbdxy8xO6iftYiU4p-cNbGuAt71mIDM',
+      origins: [req.location],
+      destinations: [req.destination]
+    }
+    const data  = GoogleDistanceApi.distance(options, (err, data) => {
+      if(err) {
+          return console.log(err);
+      }
+   
+      console.log(data);
+      res.send(data);
+  });
+};
+
 /*
 const googleMapsClient = require('@google/maps').createClient({
     key: 'AIzaSyABCbdxy8xO6iftYiU4p-cNbGuAt71mIDM'
 });
 */
 
+/*
+googleMapsClient.distanceMatrix({
+  origins: req.location,
+  destinations: req.destination
 
-exports.getDistance = (req, res) => {
-    console.log(">>>>>>>>> IN GET DISTANCE <<<<<<<<<");
+}, function(err, response) {
+  if (!err) {
+    res.status(200).json(response.json.results);
 
-    googleMapsClient.distanceMatrix({
-        origins: req.location,
-        destinations: req.destination
-
-      }, function(err, response) {
-        if (!err) {
-          res.status(200).json(response.json.results);
-
-          console.log("returned" + response.json.results)
-        } else {
-            console.log(err);
-            res.status(500).send(err);
-        }
-      });
-
-    
-};
+    console.log("returned" + response.json.results)
+  } else {
+      console.log(err);
+      res.status(500).send(err);
+  }
+});
+*/
