4010d9b78e38962b09d14eb9bf1a786225870ce1
Joseph Vincenzi <j.vincenzi@bellevuecollege.edu>
2019-12-02 02:50:34 -0800

    go

---
+++ nodeServer/models/UserCommentData.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+const UserCommentDataSchema = new Schema({
+  _id: {
+    type:String,
+    required: false
+  },
+  UserName: {
+    type:String, 
+    required: false
+  },
+  Comment: {
+    type:String, 
+    required: false
+  }
+});
+
+module.exports = mongoose.model("Comment", UserCommentDataSchema);
